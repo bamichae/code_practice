6ee5cd6f74a331e7b469071fc050723f322f57bd
ubuntu <bamichae@buffalo.edu>
2018-08-18 11:51:00 -0400

    adding permutation check

---
+++ 1_2_detect_permutation/src/com/detect_permutation/DetectPermutation.java
@@ -0,0 +1,54 @@
+package com.detect_permutation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DetectPermutation {
+
+    DetectPermutation() {
+        run();
+    }
+
+    public void run() {
+        String firstString = "abcdefghijkl";
+        String secondString = "acbdfegihljk";
+
+        System.out.println(checkForPermutation(firstString, secondString));
+    }
+
+    public boolean checkForPermutation(String firstString, String secondString) {
+        Map<Character, Integer> characterCounter = new HashMap<Character, Integer>();
+
+        if (firstString.length() != secondString.length()) {
+            return false;
+        }
+
+        //separate string into chars, put in hashmap
+        for (int i = 0; i < firstString.length(); i++) {
+            if (characterCounter.get(firstString.charAt(i)) == null)
+                characterCounter.put(firstString.charAt(i), 0);
+
+            int charCount = characterCounter.get(firstString.charAt(i));
+            characterCounter.put(firstString.charAt(i), ++charCount);
+        }
+
+        //decrement char count for each char seen
+        for (int i = 0; i < secondString.length(); i++) {
+            if (characterCounter.get(secondString.charAt(i)) == null || characterCounter.get(secondString.charAt(i)) < 0) {
+                return false;
+            }
+            int charCount = characterCounter.get(secondString.charAt(i));
+            characterCounter.put(secondString.charAt(i), --charCount);
+        }
+
+        //if all chars are 0, the strings are permutations of each other
+        for (char character : characterCounter.keySet()) {
+            if (characterCounter.get(character) != 0) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+}
---
+++ 1_2_detect_permutation/src/com/detect_permutation/Main.java
@@ -0,0 +1,8 @@
+package com.detect_permutation;
+
+public class Main {
+
+    public static void main(String[] args) {
+	DetectPermutation detectPermutation = new DetectPermutation();
+    }
+}
